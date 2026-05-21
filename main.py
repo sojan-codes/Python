bf4b4291555e13c541dd1760d54e12ccb629fb6d
Sojan <sojanshakyaa@gmail.com>
2026-05-21 08:18:47 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+userName = 'Sojan Buddhacharya'
+userAge = 20
+userEducation = 'bachelor'
+userGpa = 3.8
+
+print("Name :" , userName)
+print("Age :" , userAge)
+print("Education :" , userEducation)
+print("GPA :" , userGpa)
