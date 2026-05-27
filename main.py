6ac53389290baf6c037b7e466cc6228d934e77c1
Sojan <sojanshakyaa@gmail.com>
2026-05-27 08:06:53 +0545

    update main.py

--- main.py
+++ main.py
@@ -28,11 +28,42 @@ print(rev_string_tuple)
 print(rev_string_list)
 
 #similarly
-# normal_data = "Quest"
-# merged_list = (normal_data)
-# merged_tuple = [normal_data]
+normal_data = "Quest"
+merged_list = (normal_data)
+merged_tuple = [normal_data]
 
-# print(merged_list)
-# print(merged_tuple)
+print(merged_list)
+print(merged_tuple)
+
+#dictionaries
+my_dic = {
+    "name" : "Sojan Buddhacharya",
+    "age" : 20,
+    "class" : "bachelor 4th sem"
+}
+
+my_tuple = tuple(my_dic.items())
+my_list = list(my_dic.items())
+
+print(my_tuple)
+print(my_list)
+
+#tuple into dictionaries
+
+tuple_into_dic = (
+    ("name","Hari Bahadur"),
+    ("age", 20)
+)
+
+list_into_dic = [
+    ["name","Ram Bahadur"],
+    ["age", 21]
+]
+
+list_into_dic_data = dict(list_into_dic)
+tuple_into_dic_data = dict(tuple_into_dic)
+
+print(list_into_dic_data)
+print(tuple_into_dic_data)
 
 
